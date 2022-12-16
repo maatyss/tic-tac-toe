622146375630d059f34f5a1ffe41e243e0ae55ee
Maatyss <mvancaemelbecke@gmail.com>
2022-12-16 09:45:23 +0100

    Html reference added

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ let l3 = ['', '', ''];
 
 // Declaration du plateau
 let board = [l1, l2, l3];
+
+// Initialisation du turn et de figure
+let turn = 0
+let figure = ''
+let count = 0
+
+//Declaration des reference HTML
+const displayTurn = document.querySelector('.turn')
+const displayWinner = document.querySelector('.winner')
+const buttonReset = document.querySelector('.reset')
+
+let cellDivs = document.querySelectorAll('.cell-board');
+buttonReset.addEventListener('click', resetBoard)
+
+for (let cellDiv of cellDivs) {
+    let dataCell = cellDiv.dataset.cell;
+    cellDiv.addEventListener('click', jeu.bind(null, dataCell, cellDiv));
+}
