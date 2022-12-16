552b3fe19f1ba9c844e3b50feab5ad1068be41e4
Maatyss <mvancaemelbecke@gmail.com>
2022-12-16 09:55:02 +0100

    Win test function

--- script.js
+++ script.js
@@ -74,3 +74,48 @@ function figChoice() {
     (turn % 2) ? figure = 'O' : figure = 'X'
     return figure
 }
+
+// Fonction Test Win
+function testWin(figure) {
+    const winningLines = [
+        [l1[0], l1[1], l1[2]], [l1[0], l2[0], l3[0]],
+        [l1[0], l2[1], l3[2]], [l1[1], l2[1], l3[1]],
+        [l1[2], l2[2], l3[2]], [l1[2], l2[1], l3[0]],
+        [l2[0], l2[1], l2[2]], [l3[0], l3[1], l3[2]]
+    ];
+    const indexes = [
+        [0, 1, 2], [0, 3, 6],
+        [0, 4, 8], [1, 4, 7],
+        [2, 5, 8], [2, 4, 6],
+        [3, 4, 5], [6, 7, 8]
+    ];
+    for (let line in winningLines) {
+        count = 0
+        if (winningLines[line][0] === figure && winningLines[line][1] === figure && winningLines[line][2] === figure) {
+            displayWinner.textContent = `Joueur ${figure} à gagné`
+            displayTurn.textContent = 'Fin de partie'
+            console.log(indexes[line])
+            cellDivs[indexes[line][0]].style.backgroundColor = 'lightgreen'
+            cellDivs[indexes[line][1]].style.backgroundColor = 'lightgreen'
+            cellDivs[indexes[line][2]].style.backgroundColor = 'lightgreen'
+
+            quiboucle(function (k, l, m) {
+                if (cellDivs[k].textContent === '') {
+                    cellDivs[k].textContent = '/'
+                    cellDivs[k].style.backgroundColor = 'lightgrey'
+                }
+            })
+        } else {
+            quiboucle(function (k, l, m) {
+                if (cellDivs[k].textContent !== '/' && cellDivs[k].textContent !== '') {
+                    count++
+                }
+                if (count === 9) {
+                    console.log('match nul')
+                    displayWinner.textContent = 'Match NUL'
+                    displayTurn.textContent = 'Fin de partie'
+                }
+            })
+        }
+    }
+}
